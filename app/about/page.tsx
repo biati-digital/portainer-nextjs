d9a45034e2b41fbc2c85c47474c380042e604bd8
biati-digital <manuel@biati.digital>
2023-05-31 18:35:51 -0600

    .

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+
+export default function About() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-5xl font-bold">About</h1>
+
+            <p>Primis pharetra facilisis lorem quis penatibus ad nulla inceptos, dui per tempor taciti aliquet consequat sodales, curae tristique gravida auctor interdum malesuada sagittis. Felis pretium eros ligula natoque ad ante rutrum himenaeos, adipiscing urna mauris porta quam efficitur odio, sagittis morbi tellus nisi molestie mus faucibus. Vehicula lorem accumsan eu maximus vitae penatibus odio sagittis, viverra pulvinar orci velit fames conubia magnis arcu, curae dolor cras dictum placerat mi porttitor. Lobortis amet hac elit integer ac fermentum bibendum euismod placerat sollicitudin et, rhoncus tellus adipiscing venenatis aptent convallis suscipit pharetra taciti gravida.
+
+                Nascetur nostra fermentum eleifend nullam condimentum posuere urna molestie morbi, justo vulputate ultricies himenaeos curabitur facilisis malesuada proin scelerisque, dignissim platea libero primis aptent quis mauris volutpat sollicitudin, augue eros gravida natoque odio pellentesque mi tristique. Nisi molestie quis litora risus aenean ullamcorper sodales sit vehicula maecenas suscipit, adipiscing egestas ex porta maximus iaculis congue vestibulum augue. Ipsum curae libero risus sem ante sed fringilla, nibh praesent proin euismod sodales augue etiam metus, luctus cras dignissim nascetur vivamus mi. Enim consectetur leo maecenas est non luctus, donec rutrum justo varius sapien.</p>
+        </main>
+    )
+}
